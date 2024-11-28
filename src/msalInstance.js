99140fa4579badb87c6dbb20e9d747857af08149
Thomas <thomas.lapere@supinfo.com>
2024-11-28 22:55:34 +0100

    react add

--- src/msalInstance.js
+++
@@ -1,9 +0,0 @@
-import { PublicClientApplication } from "@azure/msal-browser";
-import { msalConfig } from "./msalConfig";
-
-export const msalInstance = new PublicClientApplication(msalConfig);
-
-// Appelle `handleRedirectPromise` une fois pour éviter l'erreur
-msalInstance.handleRedirectPromise().catch((error) => {
-  console.error("Erreur d'initialisation MSAL:", error);
-});

bf58923b8b61d05335edeae400afd7abdcd75b84
Thomas <thomas.lapere@supinfo.com>
2024-11-29 00:50:28 +0100

    clean + publi

--- src/authConfig.js
+++ src/authConfig.js
@@ -2,7 +2,7 @@ export const msalConfig = {
     auth: {
       clientId: "b4a2a829-d4ce-49b9-9341-22995e0476ba", // Remplacez par l'ID client de votre application
       authority: "https://login.microsoftonline.com/3b644da5-0210-4e60-b8dc-0beec1614542/", // Remplacez par votre ID de locataire
-      redirectUri: "http://localhost:3000/auth/callback", // URL de redirection configurée dans Azure AD
+      redirectUri: "https://gentle-wave-023be5a03.5.azurestaticapps.net/auth/callback", // URL de redirection configurée dans Azure AD
     },
     cache: {
       cacheLocation: "localStorage", // Peut être "sessionStorage"
